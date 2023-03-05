ca7f2c0f51ea5874e1209d858ca2d35d055cd00e
jc <juan.cruz.business@gmail.com>
2023-03-05 17:50:47 -0600

    make

---
+++ README.md
@@ -0,0 +1,2 @@
+# gemt_v3
+OOP focused adapation of GEMT 
---
+++ gemt_interface.cpp
@@ -0,0 +1,13 @@
+#include "gemt_interface.h"
+
+#include "Arduino.h"
+
+
+// Class impelemntaiton for gemt interface
+// create a kewords text file to have kewords highlighted in code
+// Note: https://docs.arduino.cc/learn/contributions/arduino-creating-library-guide
+
+Menu::Menu(void)
+{
+
+}
