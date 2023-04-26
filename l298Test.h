f02e7c8658c012b479928100e142a647d9356d9c
jelawless <114891697+jelawless@users.noreply.github.com>
2023-04-26 12:46:01 -0500

    Uhhhhhhh motor drive print
    Analog no worko on A1 through A4

--- l298Test.h
+++ l298Test.h
@@ -4,18 +4,57 @@
 #include "gemt_interface.h"
 
 extern void l298Test(void);
-void VoltL(void);
-void VoltR();
+
 
 GEMTtest l298Display;
 
 // Initialize pins
-const unsigned short int  L8IN1 = 34;
-const unsigned short int  L8IN2 = 36;
-const unsigned short int  L8ENA = 46;
-const unsigned short int  L8IN3 = 38;
-const unsigned short int  L8IN4 = 40;
-const unsigned short int  L8ENB = 44;
+#define in1 34
+#define in2 36
+#define enA 46
+#define in3 38
+#define in4 40
+#define enB 44
+
+
+int value_in1 = 0;
+float voltage_in1;
+int value_in2 = 0;
+float voltage_in2;
+int value_in3 = 0;
+float voltage_in3;
+int value_in4 = 0;
+float voltage_in4;
+String l1;
+String l2;
+
+void test()
+{
+  pinMode(enA, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  Serial.begin(9600);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
+void voltL()
+{
+
+  value_in1 = analogRead(A3);
+  voltage_in1 = value_in1 * 5.0/1023;
+  Serial.print("Voltage OUT1= ");
+  Serial.println(voltage_in1);
+  value_in2 = analogRead(A4);
+  voltage_in2 = value_in2 * 5.0/1023;
+  Serial.print("Voltage OUT2= ");
+  Serial.println(voltage_in2);
+
+}
 
 void runl298Test(void)
 {
@@ -40,69 +79,69 @@ unsigned long l298nPreviousMillis = 0.00;
 
 extern void l298Test(void)
 {
-  int x = 1;
-  int value_in1; // Voltage readings from left output. Front & backward = in1 & in2
-  float voltage_in1;
-  int value_in2;
-  float voltage_in2;
-  String l1;
-  String l2;
-  digitalWrite(L8IN1, LOW);
-  digitalWrite(L8IN2, LOW);
-  digitalWrite(L8IN3, LOW);
-  digitalWrite(L8IN4, LOW);
+  int x=1;
   
-  if (x<4)
+  while (x<4)
    {
     switch (x) 
     {
   case 1:
-    analogWrite(L8ENA, 255);
-    digitalWrite(L8IN1, HIGH);
-    digitalWrite(L8IN2, LOW);
-    value_in1 = analogRead(A1); // Voltage readings from left output. Front & backward = in1 & in2
+    
+    analogWrite(enA, 255);
+    digitalWrite(in1, HIGH);
+    digitalWrite(in2, LOW);
+    value_in1 = analogRead(A3); // Voltage readings from left output. Front & backward = in1 & in2
     voltage_in1 = value_in1 * 5.0/1023;
-    value_in2 = analogRead(A2);
+    value_in2 = analogRead(A4);
     voltage_in2 = value_in2 * 5.0/1023;
 
     l1 = String("V-Left1: ") + String(voltage_in1, 2);
     l2 = String("V-Left2: ") + String(voltage_in2, 2);
     
+    l298Display.setStaticTestFeedbackLine("Forward");
     l298Display.setStaticTestFeedbackLine(l1);
     l298Display.setStaticTestFeedbackLine(l2);
     l298Display.showStaticTestFeedback(2000);
 
+    
+    
+    
+ 
     x=x+1;
     break;
   case 2:
-    analogWrite(L8ENA, 255);
-    digitalWrite(L8IN1, HIGH);
-    digitalWrite(L8IN2, LOW);
-    value_in1 = analogRead(A1); // Voltage readings from left output. Front & backward = in1 & in2
+    
+    analogWrite(enA, 255);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, HIGH);
+    value_in1 = analogRead(A3); // Voltage readings from left output. Front & backward = in1 & in2
     voltage_in1 = value_in1 * 5.0/1023;
-    value_in2 = analogRead(A2);
+    value_in2 = analogRead(A4);
     voltage_in2 = value_in2 * 5.0/1023;
 
     l1 = String("V-Left1: ") + String(voltage_in1, 2);
     l2 = String("V-Left2: ") + String(voltage_in2, 2);
     
+    l298Display.setStaticTestFeedbackLine("Backwards");
     l298Display.setStaticTestFeedbackLine(l1);
     l298Display.setStaticTestFeedbackLine(l2);
     l298Display.showStaticTestFeedback(2000);
-
     x=x+1;
     break;
    case 3:
-    analogWrite(L8ENA, 255);
-    digitalWrite(L8IN1, LOW);
-    digitalWrite(L8IN2, HIGH);
-    value_in1 = analogRead(A1); // Voltage readings from left output. Front & backward = in1 & in2
+    
+    analogWrite(enA, 255);
+    digitalWrite(in1, LOW);
+    digitalWrite(in2, LOW);
+    value_in1 = analogRead(A3); // Voltage readings from left output. Front & backward = in1 & in2
     voltage_in1 = value_in1 * 5.0/1023;
-    value_in2 = analogRead(A2);
+    value_in2 = analogRead(A4);
     voltage_in2 = value_in2 * 5.0/1023;
+
     l1 = String("V-Left1: ") + String(voltage_in1, 2);
     l2 = String("V-Left2: ") + String(voltage_in2, 2);
     
+    l298Display.setStaticTestFeedbackLine("OFF");
     l298Display.setStaticTestFeedbackLine(l1);
     l298Display.setStaticTestFeedbackLine(l2);
     l298Display.showStaticTestFeedback(2000);
@@ -110,52 +149,10 @@ extern void l298Test(void)
     x=x+1;
     break;
    case 4:
-    analogWrite(L8ENA, 255);
-    digitalWrite(L8IN1, LOW);
-    digitalWrite(L8IN2, HIGH);
-    value_in1 = analogRead(A1); // Voltage readings from left output. Front & backward = in1 & in2
-    voltage_in1 = value_in1 * 5.0/1023;
-    value_in2 = analogRead(A2);
-    voltage_in2 = value_in2 * 5.0/1023;
-    l1 = String("V-Left1: ") + String(voltage_in1, 2);
-    l2 = String("V-Left2: ") + String(voltage_in2, 2);
-    
-    l298Display.setStaticTestFeedbackLine(l1);
-    l298Display.setStaticTestFeedbackLine(l2);
+    l298Display.setStaticTestFeedbackLine("Null");
     l298Display.showStaticTestFeedback(2000);
-
     x=5;
     break;
     }
    }
-}
-
-void VoltL()
-{
-  analogWrite(L8ENA, 255);
-  digitalWrite(L8IN1, HIGH);
-  digitalWrite(L8IN2, LOW);
-  delay(500);
-  VoltL();
-  digitalWrite(L8IN1, LOW);
-  digitalWrite(L8IN2, HIGH);
-  delay(500);
-  VoltL();
-  digitalWrite(L8IN1, LOW);
-  digitalWrite(L8IN2, LOW);
-}
-
-void VoltR()
-{
-  analogWrite(L8ENB, 255);
-  digitalWrite(L8IN3, HIGH);
-  digitalWrite(L8IN4, LOW);
-  delay(500);
-  VoltR();
-  digitalWrite(L8IN3, LOW);
-  digitalWrite(L8IN4, HIGH);
-  delay(500);
-  VoltR();
-  digitalWrite(L8IN1, LOW);
-  digitalWrite(L8IN2, LOW);
 }
